8cb7daa93e3b22c1c540e74624af56853e44273f
Roshan Kumar Gupta <128606882+rk-roshan-kr@users.noreply.github.com>
2025-11-20 01:48:42 +0530

    Replace HashRouter with BrowserRouter in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom';
+import App from './App.tsx'
 import './index.css'
-import App from './App'
-
-import ErrorBoundary from './components/common/ErrorBoundary'
+import { BrowserRouter } from 'react-router-dom' // <--- USE THIS
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* Use HashRouter instead of BrowserRouter */}
-    <HashRouter>
+    <BrowserRouter> {/* <--- NOT HashRouter */}
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
-
-
